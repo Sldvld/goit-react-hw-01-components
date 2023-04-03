02e8013b7522205f58478b086a3637597561b64c
Sldvlld <sladvlad@gmail.com>
2023-04-03 23:15:33 +0300

    add profile

---
+++ src/components/App/App/App.jsx
@@ -0,0 +1,17 @@
+import { Profile } from '../Profile/Profile';
+import user from 'data/user.json';
+import css from './App.module.css';
+
+export const App = () => {
+  return (
+    <div className={css.container}>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+};
